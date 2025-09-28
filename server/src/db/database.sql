d652148da4e5e623ee89557f9fda67615e624116
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2025-09-28 20:01:36 -0300

    Novas colunas das tabelas de eventos e voluntários

--- server/src/db/database.sql
+++ server/src/db/database.sql
@@ -2,22 +2,43 @@ CREATE DATABASE prova_p1;
 
 USE prova_p1;
 
-CREATE TABLE users(
-    id auto_increment,
+CREATE TABLE usuarios(
+    id int auto_increment,
     nome varchar(200) NOT NULL,
     email varchar(200) NOT NULL,
     senha varchar(200) NOT NULL
 );
 
 -- seeder para usuário adm inicial
-INSERT INTO users
+INSERT INTO usuarios
 (nome, email, senha)
 VALUES ('Administrador', 'admin@ifrs.edu.br', '123456');
 
-CREATE TABLE events(
+CREATE TABLE eventos(
+    id int auto_increment,
+    nome varchar(200) NOT NULL,
+    descricao varchar(200) NOT NULL,
+    data_hora_inicio datetime NOT NULL,
+    data_hora_fim datetime NOT NULL,
 
+    PRIMARY KEY id
 );
 
-CREATE TABLE volunteers(
+CREATE TABLE voluntarios(
+    id int auto_increment,
+    cpf char(11) NOT NULL,
+    nome varchar(200) NOT NULL,
+    email varchar(200) NOT NULL,
+    telefone varchar(200) NOT NULL,
 
+    PRIMARY KEY id
+);
+
+CREATE TABLE voluntario_eventos(
+    id int auto_increment,
+    evento_id bigint NOT NULL,
+    voluntario_id bigint NOT NULL,
+
+    FOREIGN KEY evento_id REFERENCES eventos(id),
+    FOREIGN KEY voluntario_id REFERENCES voluntarios(id)
 );
