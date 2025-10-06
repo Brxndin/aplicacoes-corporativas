72a7270b5ea401387382c5fcb36a0b0256aa12b1
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2025-10-05 21:53:42 -0300

    Criação do dashboard

---
+++ app/src/pages/Dashboard.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../auth/AuthContext";
+import api from "../config/api";
+
+export default function Dashboard() {
+  const { authState } = useAuth();
+  const [dashboardText, setDashboardText] = useState("");
+
+  useEffect(() => {
+    api
+      .get("/dashboard")
+      .then((res) => {
+        setDashboardText(res.data.text);
+      })
+      .catch((error) => {
+        let dadosRetorno = error.response.data;
+
+        console.log(dadosRetorno);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Olá, {authState.user?.name}!</h2>
+      <p>{dashboardText}</p>
+    </div>
+  );
+}
