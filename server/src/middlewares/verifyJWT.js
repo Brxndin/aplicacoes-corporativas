3a1234071aa3de4afba329538e55619b8195b7fa
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2025-10-12 14:39:19 -0300

    Correção de erro

--- server/src/middlewares/verifyJWT.js
+++ server/src/middlewares/verifyJWT.js
@@ -11,30 +11,30 @@ function verifyJWT(req, res, next)
         res.status(401).json({
             message: 'É preciso informar o token de acesso!',
         });
+    } else {
+        // aqui substitui pois o header vem em texto puro
+        const token = authHeader.replace('Bearer ', '');
+
+        try {
+            // Verifica se o token é valido
+            jwt.verify(token, process.env.JWT_SECRET, (error, payload) => {
+                if (error) {
+                    throw new error;
+                }
+
+                // aqui pega as informações do token que foram salvas no login
+                // é usado posteriormente quando precisa saber se o usuário é adm
+                req.userPayload = payload;
+            });
+
+            next();
+        } catch (error) {
+            // aqui usa status específico para que o front capture o erro
+            res.status(401).json({
+                message: `Falha ao autenticar o token: ${error.message}`,
+            });
+        }
     }
-
-    // aqui substitui pois o header vem em texto puro
-    const token = authHeader.replace('Bearer ', '');
-
-    try {
-        // Verifica se o token é valido
-        jwt.verify(token, process.env.JWT_SECRET, (error, payload) => {
-            if (error) {
-                throw new error;
-            }
-
-            // aqui pega as informações do token que foram salvas no login
-            // é usado posteriormente quando precisa saber se o usuário é adm
-            req.userPayload = payload;
-        });
-
-        next();
-    } catch (error) {
-        // aqui usa status específico para que o front capture o erro
-        res.status(401).json({
-            message: `Falha ao autenticar o token: ${error.message}`,
-        });
-  }
 }
 
 export default verifyJWT;
