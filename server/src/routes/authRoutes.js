6150afec2529459a62bbc8510e5b15d92ec742e7
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2025-10-11 21:30:13 -0300

    Correções no Swagger

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -6,24 +6,16 @@ const router = express.Router();
 
 /**
  * @swagger
- * tags:
- *      - Authenticator
- * name: Authenticator
- * description: Gerenciamento de login e autenticação
- */
-
-/**
- * @swagger
  * /auth/login:
- * post:
- *      summary: Realizar o login do usuário
- *      tags:
- *            - Authenticator
- *      responses:
- *          201:
- *              description: Login realizado com sucesso.
- *          500:
- *              description: Erro ao realizar o login.
+ *      post:
+ *          summary: Realizar o login do usuário
+ *          tags:
+ *              - Authenticator
+ *          responses:
+ *              201:
+ *                  description: Login realizado com sucesso.
+ *              500:
+ *                  description: Erro ao realizar o login.
  */
 router.post('/login', AuthController.login);
 
--- server/src/routes/eventsRoutes.js
+++ server/src/routes/eventsRoutes.js
@@ -1,30 +1,21 @@
 import express from 'express';
 import EventsController from '../controllers/eventsController.js';
 import verifyAdmin from '../middlewares/verifyAdmin.js';
-import verifyJWT from '../middlewares/verifyJWT.js';
 
 const router = express.Router();
 
 /**
  * @swagger
- * tags:
- *      - Eventos
- * name: Eventos
- * description: Gerenciamento de eventos
- */
-
-/**
- * @swagger
  * /events:
- * get:
- *      summary: Obter todos os eventos
- *      tags:
- *          - Eventos
- *      responses:
- *          200:
- *              description: Todos os eventos encontrados.
- *          500:
- *              description: Erro ao buscar os dados.
+ *      get: 
+ *          summary: Obter todos os eventos
+ *          tags:
+ *              - Eventos
+ *          responses:
+ *              200:
+ *                  description: Todos os eventos encontrados.
+ *              500:
+ *                  description: Erro ao buscar os dados.
  */
 router.get('/', EventsController.getAll);
 router.get('/:id', EventsController.getOne);
@@ -32,15 +23,15 @@ router.get('/:id', EventsController.getOne);
 /**
  * @swagger
  * /events:
- * post:
- *      summary: Inserir novo evento
- *      tags:
- *          - Eventos
- *      responses:
- *          201:
- *              description: Evento criado com sucesso.
- *          500:
- *              description: Erro ao cadastrar os dados.
+ *      post:
+ *          summary: Inserir novo evento
+ *          tags:
+ *              - Eventos
+ *          responses:
+ *              201:
+ *                  description: Evento criado com sucesso.
+ *              500:
+ *                  description: Erro ao cadastrar os dados.
  */
 router.post('/', verifyAdmin, EventsController.create);
 router.put('/:id', EventsController.update);
