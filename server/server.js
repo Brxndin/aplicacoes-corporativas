094808700004b0923ae9f70e6b4a349fe6e02024
Brxndin <brandongoncalvesdeoliveira@gmail.com>
2025-11-16 22:33:28 -0300

    Comentário

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import { app, logger } from './src/app.js';
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
 
+// lembrar de fazer a configuração para uma build mais leve usando o webpack ou esbuild e afins
+
 // inicia o servidor
 app.listen(PORT, () => {
     logger.info(`Servidor rodando em: http://${HOST}:${PORT}`);
